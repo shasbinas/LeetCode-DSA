5c4a2e2d39b1660ab915e7d19ccda75ee8d95076
Shasbin  AS <shasbinas1@gmail.com>
2025-09-01 23:08:04 +0530

    Time: 1 ms (19.66%), Space: 54 MB (43.25%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,13 @@
+function strStr(haystack, needle) {
+    if (needle.length === 0) return 0;
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let j = 0;
+        while (j < needle.length && haystack[i + j] === needle[j]) {
+            j++;
+        }
+        if (j === needle.length) {
+            return i; 
+        }
+    }
+    return -1;
+}

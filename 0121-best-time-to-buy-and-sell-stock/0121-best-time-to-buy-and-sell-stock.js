d9167e78f655929438019ae78847803bf06e4e7c
Shasbin  AS <shasbinas1@gmail.com>
2025-09-11 13:09:52 +0530

    Time: 6 ms (15.89%), Space: 64.1 MB (94.22%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+let maxProfit = function(prices) {
+  let minPrice = Infinity; 
+  let maxProfit = 0;   
+  for (let price of prices) {
+    if (price < minPrice) {
+      minPrice = price;
+    } else {
+      maxProfit = Math.max(maxProfit, price - minPrice);
+    }
+  }
+
+  return maxProfit;
+};

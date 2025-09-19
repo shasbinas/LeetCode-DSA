1e16bb31a0a70b90f4140130a14f6766eee61ee9
Shasbin  AS <shasbinas1@gmail.com>
2025-09-19 11:56:35 +0530

    Time: 4 ms (16.79%), Space: 58.8 MB (91.27%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,13 @@
+function findMaxConsecutiveOnes(nums) {
+    let maxCount = 0;
+    let currentCount = 0;
+    for (let num of nums) {
+        if (num === 1) {
+            currentCount++;
+            maxCount = Math.max(maxCount, currentCount);
+        } else {
+            currentCount = 0;
+        }
+    }
+    return maxCount;
+}

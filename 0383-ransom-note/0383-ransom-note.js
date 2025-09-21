392c8773ee719e6b26c5c8358041f9b84ba60561
Shasbin  AS <shasbinas1@gmail.com>
2025-09-21 11:08:38 +0530

    Time: 20 ms (44.15%), Space: 59.1 MB (44%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,15 @@
+function canConstruct(ransomNote, magazine) {
+  const count = {};
+  for (let ch of magazine) {
+    count[ch] = (count[ch] || 0) + 1;
+  }
+  for (let ch of ransomNote) {
+    if (!count[ch]) {
+      return false;  
+    }
+    count[ch]--;  
+  }
+
+  return true;
+    
+};

c9386e6c202b7d2f2cdb233a82b1abb007192427
Shasbin  AS <shasbinas1@gmail.com>
2025-09-23 22:15:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -0,0 +1,18 @@
+function longestPalindrome(s) {
+    const charCount = {};
+  for (let char of s) { 
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    let length = 0;
+    let hasOdd = false;
+
+    for (let count of Object.values(charCount)) {
+        if (count % 2 === 0) {
+            length += count;
+        } else {
+            length += count - 1; 
+            hasOdd = true;  
+    }
+    if (hasOdd) length += 1;
+    return length;
+}

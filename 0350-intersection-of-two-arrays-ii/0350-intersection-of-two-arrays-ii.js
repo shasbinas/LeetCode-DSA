65da2467600220836e4ed4dd34e4c06c7c446035
Shasbin  AS <shasbinas1@gmail.com>
2025-10-02 20:27:55 +0530

    Time: 2 ms (60.34%), Space: 56.1 MB (36.3%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,14 @@
+function intersect(nums1, nums2) {
+  const map = new Map();
+  const result = [];
+  for (let num of nums1) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+  for (let num of nums2) {
+    if (map.get(num) > 0) {
+      result.push(num);
+      map.set(num, map.get(num) - 1);
+    }
+  }
+  return result;
+}

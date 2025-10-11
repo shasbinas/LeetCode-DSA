0dabc54a22fa693bf80a8649f4e4e9f8d1ab9013
Shasbin  AS <shasbinas1@gmail.com>
2025-10-11 13:40:00 +0530

    Time: 7 ms (80.26%), Space: 71.1 MB (86.3%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -1,8 +1,15 @@
 function findDisappearedNumbers(nums) {
-  const n = nums.length;
-  const fullSet = new Set(Array.from({ length: n }, (_, i) => i + 1));
-  for (let num of nums) {
-    fullSet.delete(num);
+  for (let i = 0; i < nums.length; i++) {
+    const index = Math.abs(nums[i]) - 1;
+    if (nums[index] > 0) {
+      nums[index] = -nums[index];
+    }
   }
-  return Array.from(fullSet);
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+  return result;
 }

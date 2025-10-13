0f4b038630eb97110e879e4544b4d9ef946ef55c
Shasbin  AS <shasbinas1@gmail.com>
2025-10-13 15:55:41 +0530

    Time: 0 ms (100%), Space: 55.8 MB (16.23%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,8 @@
+function detectCapitalUse(word) { 
+  if (word === word.toUpperCase()) return true;
+  if (word === word.toLowerCase()) return true;
+  if (word[0] === word[0].toUpperCase() && word.slice(1) === word.slice(1).toLowerCase()) {
+    return true;
+  }
+  return false;
+}

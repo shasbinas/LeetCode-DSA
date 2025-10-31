178d84a7d81b87d5fcf57d5c19369655888358d0
Shasbin  AS <shasbinas1@gmail.com>
2025-10-31 19:15:09 +0530

    Time: 36 ms (40.03%), Space: 59.7 MB (41.22%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,12 @@
+function firstUniqChar(s) {
+  const count = {};
+  for (let char of s) {
+    count[char] = (count[char] || 0) + 1;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (count[s[i]] === 1) {
+      return i;
+    }
+  }
+  return -1; 
+}

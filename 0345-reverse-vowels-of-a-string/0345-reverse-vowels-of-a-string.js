30bfd3b23be5708062e44de0d99692e1ba63223b
Shasbin  AS <shasbinas1@gmail.com>
2025-11-06 10:27:21 +0530

    Time: 2 ms (98.25%), Space: 59.2 MB (77.94%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -1,19 +1,12 @@
-function reverseVowels(s) {
-  const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
-  let arr = s.split('');
-  let left = 0, right = arr.length - 1;
-  while (left < right) {
-    while (left < right && !vowels.has(arr[left])) {
-      left++;
-    }
-    while (left < right && !vowels.has(arr[right])) {
-      right--;
-    }
-    if (left < right) {
-      [arr[left], arr[right]] = [arr[right], arr[left]];
-      left++;
-      right--;
-    }
+function reverseVowels(s){
+  const v=new Set(['a','e','i','o','u','A','E','I','O','U'])
+  const a=s.split('')
+  let l=0,r=a.length-1
+  while(l<r){
+    while(l<r&&!v.has(a[l]))l++
+    while(l<r&&!v.has(a[r]))r--
+    ;[a[l],a[r]]=[a[r],a[l]]
+    l++;r--
   }
-  return arr.join('');
+  return a.join('')
 }

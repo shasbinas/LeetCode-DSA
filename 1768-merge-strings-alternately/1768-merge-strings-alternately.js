3076a99e9628051f8b03dba56d09364457995bf4
Shasbin  AS <shasbinas1@gmail.com>
2025-11-12 11:13:55 +0530

    Time: 43 ms (79.63%), Space: 53.9 MB (90.69%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -1,9 +1,8 @@
 function mergeAlternately(word1, word2) {
-  let merged = '';
-  const maxLen = Math.max(word1.length, word2.length);
-  for (let i = 0; i < maxLen; i++) {
-    if (i < word1.length) merged += word1[i];
-    if (i < word2.length) merged += word2[i];
+  let result = "";
+  let i = 0, j = 0;
+  while (i < word1.length && j < word2.length) {
+    result += word1[i++] + word2[j++];
   }
-  return merged;
+  return result + word1.slice(i) + word2.slice(j);
 }

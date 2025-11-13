b9be6126f7b0710a4c83f5e681603650b4745c74
Shasbin  AS <shasbinas1@gmail.com>
2025-11-13 14:18:11 +0530

    Time: 0 ms (100%), Space: 58.6 MB (22.14%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,19 @@
+function compress(chars){
+  let write = 0;
+  let read = 0;
+  while(read < chars.length){
+    let char = chars[read];
+    let count = 0;
+    while(read < chars.length && chars[read] === char){
+      read++;
+      count++;
+    }
+    chars[write++] = char;
+    if(count > 1){
+      for(let c of String(count)){
+        chars[write++] = c;
+      }
+    }
+  }
+  return write;
+}

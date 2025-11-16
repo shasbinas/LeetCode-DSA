c70420c2de6440bac24cb343ad56ffb3aa3de027
Shasbin  AS <shasbinas1@gmail.com>
2025-11-16 19:24:36 +0530

    Time: 63 ms (48.65%), Space: 129 MB (34.36%) - LeetHub

---
+++ 0077-combinations/0077-combinations.js
@@ -0,0 +1,13 @@
+var combine=function(n,k){
+  let res=[],path=[];
+  function backtrack(start){
+    if(path.length===k){res.push([...path]);return;}
+    for(let i=start;i<=n;i++){
+      path.push(i);
+      backtrack(i+1);
+      path.pop();
+    }
+  }
+  backtrack(1);
+  return res;
+};

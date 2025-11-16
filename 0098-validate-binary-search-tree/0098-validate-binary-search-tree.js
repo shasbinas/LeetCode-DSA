a668d11492074ed23172e26021da8f32d64ab917
Shasbin  AS <shasbinas1@gmail.com>
2025-11-16 19:25:43 +0530

    Time: 1 ms (62.14%), Space: 59 MB (45.35%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,8 @@
+var isValidBST=function(root){
+  function dfs(node,min,max){
+    if(!node)return true;
+    if(node.val<=min||node.val>=max)return false;
+    return dfs(node.left,min,node.val)&&dfs(node.right,node.val,max);
+  }
+  return dfs(root,-Infinity,Infinity);
+};

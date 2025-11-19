e8fdbc74c43ef88fc4491d0531cd0fb8af6f49f9
Shasbin  AS <shasbinas1@gmail.com>
2025-11-19 10:44:05 +0530

    Time: 12 ms (89.25%), Space: 58.2 MB (65.71%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,21 @@
+var fourSum=function(nums,target){
+    nums.sort((a,b)=>a-b);
+    const res=[];
+    for(let i=0;i<nums.length-3;i++){
+        if(i>0&&nums[i]===nums[i-1])continue;
+        for(let j=i+1;j<nums.length-2;j++){
+            if(j>i+1&&nums[j]===nums[j-1])continue;
+            let l=j+1,r=nums.length-1;
+            while(l<r){
+                const sum=nums[i]+nums[j]+nums[l]+nums[r];
+                if(sum===target){
+                    res.push([nums[i],nums[j],nums[l],nums[r]]);
+                    while(l<r&&nums[l]===nums[l+1])l++;
+                    while(l<r&&nums[r]===nums[r-1])r--;
+                    l++;r--;
+                }else if(sum<target)l++;else r--;
+            }
+        }
+    }
+    return res;
+}

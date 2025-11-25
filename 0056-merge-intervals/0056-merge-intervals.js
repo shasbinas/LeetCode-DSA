0db47be78712da86a76780c224975614124f8789
Shasbin  AS <shasbinas1@gmail.com>
2025-11-25 13:03:13 +0530

    Time: 11 ms (36.23%), Space: 63.6 MB (87.85%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,12 @@
+var merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    let merged = [];
+    for (let interval of intervals) {
+        if (!merged.length || merged[merged.length - 1][1] < interval[0]) {
+            merged.push(interval);
+        } else {
+            merged[merged.length - 1][1] = Math.max(merged[merged.length - 1][1], interval[1]);
+        }
+    }
+    return merged;
+};

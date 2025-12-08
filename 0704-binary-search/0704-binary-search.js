196917fb706b4e6ed85ec3ba6b2e993ca2cdfb9d
Shasbin  AS <shasbinas1@gmail.com>
2025-12-08 20:08:26 +0530

    Time: 0 ms (100%), Space: 57.9 MB (40.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -1,10 +1,10 @@
-function search(nums,target){
-  let l=0,r=nums.length-1
-  while(l<=r){
-    const m=Math.floor((l+r)/2)
-    if(nums[m]===target)return m
-    if(nums[m]<target)l=m+1
-    else r=m-1
-  }
-  return -1
-}
+var search=function(nums,target){
+    let l=0,r=nums.length-1;
+    while(l<=r){
+        let m=(l+r>>1);
+        if(nums[m]==target)return m;
+        if(nums[m]<target)l=m+1;
+        else r=m-1;
+    }
+    return -1;
+};

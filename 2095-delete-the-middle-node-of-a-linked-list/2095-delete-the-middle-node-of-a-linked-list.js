2f4c5a68ae2cffc60003f637b42cbdbbb06716d7
Shasbin  AS <shasbinas1@gmail.com>
2025-12-29 15:07:41 +0530

    Time: 3 ms (92.67%), Space: 98.3 MB (82.33%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -0,0 +1,11 @@
+deleteMiddle = head => {
+  if (!head || !head.next) return null
+  let slow = head, fast = head, prev = null
+  while (fast && fast.next) {
+    prev = slow
+    slow = slow.next
+    fast = fast.next.next
+  }
+  prev.next = slow.next
+  return head
+}

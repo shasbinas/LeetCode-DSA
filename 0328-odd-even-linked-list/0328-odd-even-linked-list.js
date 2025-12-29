1015ded2130df74d09f65138ab25767b37b23cea
Shasbin  AS <shasbinas1@gmail.com>
2025-12-29 15:08:15 +0530

    Time: 0 ms (100%), Space: 58.9 MB (68.91%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -0,0 +1,12 @@
+oddEvenList = head => {
+  if (!head) return null
+  let odd = head, even = head.next, evenHead = even
+  while (even && even.next) {
+    odd.next = even.next
+    odd = odd.next
+    even.next = odd.next
+    even = even.next
+  }
+  odd.next = evenHead
+  return head
+}

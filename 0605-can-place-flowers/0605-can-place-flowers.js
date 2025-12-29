f9300a21b2067f40caf5a462f19f5b98ef9b8421
Shasbin  AS <shasbinas1@gmail.com>
2025-12-29 14:46:54 +0530

    Time: 2 ms (59.37%), Space: 57.5 MB (23.55%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -1,8 +1,13 @@
-/**
- * @param {number[]} flowerbed
- * @param {number} n
- * @return {boolean}
- */
-var canPlaceFlowers = function(flowerbed, n) {
-    
-};
+canPlaceFlowers = (f, n) => {
+  for (let i = 0; i < f.length && n > 0; i++) {
+    if (
+      f[i] === 0 &&
+      (i === 0 || f[i - 1] === 0) &&
+      (i === f.length - 1 || f[i + 1] === 0)
+    ) {
+      f[i] = 1
+      n--
+    }
+  }
+  return n === 0
+}

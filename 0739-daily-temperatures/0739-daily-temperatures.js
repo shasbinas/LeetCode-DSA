8e71f7de3555933efa1bf1199637e05b7a4ae268
Shasbin  AS <shasbinas1@gmail.com>
2025-12-30 14:10:40 +0530

    Time: 19 ms (55.65%), Space: 78.9 MB (29.8%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -1,11 +1,11 @@
-dailyTemperatures = T => {
-  let n = T.length, res = Array(n).fill(0), stack = []
-  for (let i = 0; i < n; i++) {
-    while (stack.length && T[i] > T[stack[stack.length-1]]) {
-      let idx = stack.pop()
-      res[idx] = i - idx
-    }
-    stack.push(i)
-  }
-  return res
+var dailyTemperatures=function(t){
+let n=t.length,res=Array(n).fill(0),stack=[]
+for(let i=0;i<n;i++){
+while(stack.length&&t[i]>t[stack[stack.length-1]]){
+let idx=stack.pop()
+res[idx]=i-idx
+}
+stack.push(i)
+}
+return res
 }
